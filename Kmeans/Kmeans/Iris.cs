57f01afa6cc7d0955235d5710f257a4f2057ca6c
Hacenee <kadouche.hacenee@gmail.com>
2016-04-01 11:43:43 +0200

    algorithm kmeans in c#

---
+++ Kmeans/Kmeans/Iris.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KMeans
+{
+    class Iris
+    {
+        private double v1;
+        private double v2;
+        private double v3;
+        private double v4;
+        private string label;
+
+        public Iris(double v1, double v2, double v3, double v4, string label)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+            this.v4 = v4;
+            this.v3 = v3;
+            this.label = label;
+        }
+
+        public double Distance(Iris b)
+        {
+            // A compléter
+            return 0;
+        }
+
+        public override string ToString()
+        {
+            return "(" + v1 + " " + v2 + " " + v3 + " " + v4 + ") ; " + this.label;
+        }
+
+    }
+}
---
+++ Kmeans/Kmeans/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
+using System.Globalization;
+
+namespace KMeans
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            #region LectureFichier
+
+            // Lire le fichier csv
+            string[] allLines = File.ReadAllLines(@"iris.csv");
+
+            // Chaque ligne est un individu
+            Iris[] data = new Iris[allLines.Count()];
+
+            // Pour chaque ligne, convertir les données
+            for (int i = 0; i < allLines.Count(); i++)
+            {
+                // couper suivant le séparateur ,
+                string[] tmp = allLines[i].Split(',');
+                double v1 = Convert.ToDouble(tmp[0], CultureInfo.InvariantCulture);
+                double v2 = Convert.ToDouble(tmp[1], CultureInfo.InvariantCulture);
+                double v3 = Convert.ToDouble(tmp[2], CultureInfo.InvariantCulture);
+                double v4 = Convert.ToDouble(tmp[3], CultureInfo.InvariantCulture);
+                data[i] = new Iris(v1, v2, v3, v4, tmp[4]);
+            }
+
+            #endregion
+
+            int[] cluster = new int[allLines.Count()]; // cluster[i] sera l'index du cluster de l'individu i
+            int k = 3; // Nombre de clusters 
+            Iris[] seeds = new Iris[k]; // Les clusters
+
+            for (int i = 0; i < k; i++)
+            {
+                seeds[i] = new Iris(0, 0, 0, 0, "seed"); // A tirer aléatoirement
+            }
+
+            // Ici la méthode K-Means 
+            // ...
+
+
+            // Affichage
+            for (int i = 0; i < allLines.Count(); i++)
+            {
+                Console.WriteLine(data[i] + " dans le cluster : " + cluster[i]);
+            }
+        }
+    }
+}
